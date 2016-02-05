42dcf7c9ecc6d2f4d8f1c7cff093d9f48ff88970
Leonardo Saraiva <vyper@maneh.org>
2016-02-04 22:14:40 -0200

    Fixing specs for new features on lotus-validations

--- spec/fixtures/with_validation_length_model.rb
+++ spec/fixtures/with_validation_length_model.rb
@@ -2,5 +2,5 @@ class WithValidationLengthModel
   include Lotus::Validations
 
   attribute :name_equal_10,     size: 10
-  attribute :name_between_1_10, size: 1..10
+  attribute :name_between_2_10, size: 2..10
 end
